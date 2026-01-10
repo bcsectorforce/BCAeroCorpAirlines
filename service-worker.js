ea5afccd6439393c050e622b5fdea331668737b1
bcsectorforce <bellybutton7@proton.me>
2026-01-10 15:05:54 -0500

    Add service worker for asset caching
    Implement a service worker for caching assets.

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'bc-aerocorp-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
